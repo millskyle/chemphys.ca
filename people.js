596540d40b2020ce3eb4860b0c74ef977f9c1888
mspanner <33126312+mspanner@users.noreply.github.com>
2017-10-31 14:56:53 -0400

    Update people.js

--- people.js
+++ people.js
@@ -73,7 +73,7 @@ app.controller('myCtrl', function($scope, $location) {
             },
             {
                 name: "Khabat Heshami",
-                photo: "https://chemphys.ca/pages/people_photos/khabat.jpg",
+                photo: "https://chemphys.ca/pages/people_photos/KhabatHeshami.png",
                 email: "khabat.heshami@nrc.ca",
                 status: "Research Assistant",
 	    	rank: "Research Officer",
